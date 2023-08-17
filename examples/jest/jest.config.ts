826a2e95ebdd2ce2cd5e136fd9b1b9667f9c1f41
YutaUra <yuuta3594@gmail.com>
2023-08-17 19:40:04 +0900

    update

--- examples/jest/jest.config.ts
+++
@@ -1,6 +0,0 @@
-import { JestConfigWithTsJest } from "ts-jest";
-
-export default {
-  preset: "ts-jest",
-  testEnvironment: "@firestore-emulator/jest/environment/node",
-} satisfies JestConfigWithTsJest;
